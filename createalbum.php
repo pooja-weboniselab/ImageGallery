76a1b3bfb64628073da9865aa1c8f5dde6d226c5
pooja-weboniselab <pooja.sinha@weboniselab.com>
2013-12-05 18:30:56 +0530

    album creation

---
+++ createalbum.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: webonise
+ * Date: 28/11/13
+ * Time: 5:19 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ */
+session_start();
+
+$dbHost = "localhost";
+$dbUser = "poojawebonise";
+$dbPass = "weboniselab";
+$dbName = "imagegallery_db";
+
+/* connecting databases */
+$mysql = mysql_connect($dbHost, $dbUser, $dbPass);
+mysql_select_db($dbName);
+$query = "select * from imagemaster" ;
+$testData=  mysql_query($query);
+// $data1=  mysql_fetch_array($tesdata);
+$data = array();
+$n=0 ;
+
+while($imagedata=mysql_fetch_array($testData,MYSQL_ASSOC)){
+    $data[]=$imagedata;
+    $n++ ;
+}
+
+
+
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Admin Dashboard<?php echo $_SESSION['id'] ;?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="bootstrap/css/custome.css" rel="stylesheet" media="screen">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+
+    <!--<link href="bootstrap/css/bootswatch.css" rel="stylesheet" media="screen">-->
+</head>
+<body style="padding-top: 60px;">
+<div class="navbar">
+    <div class="navbar-inner">
+        <a class="brand" href="#">Image Gallery</a>
+        <ul class="nav">
+            <li class="active"><a href="#">upload Image</a></li>
+            <li><a href="#">create Album</a></li>
+            <li><a href="logout.php">logout</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class ="span10">
+            <h3>Album</h3>
+        </div>
+        <div class="span2">
+            <button class=" "onclick="createAlbum()" >create Album</button>
+            <input type="hidden" id="album">
+        </div>
+    </div>
+    <div class="row">
+        <div class = "span12">
+            <ul id="albumgallery">
+
+            </ul>
+        </div>
+
+    </div>
+
+</div>
+</body>
+
+    <script type="text/javascript">
+        function createAlbum()
+        {
+            var x;
+
+            var person=prompt("Album name","");
+
+            if (person!=null)
+            {
+                x= person ;
+                insertAlbum(x);
+
+
+            }
+        }
+
+        function insertAlbum(str)
+        {
+                alert(str);
+            if (window.XMLHttpRequest)
+            {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            }
+            else
+            {// code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            alert(xmlhttp);
+           xmlhttp.open("GET","newalbum.php?album="+str,true);
+            xmlhttp.send();
+        }
+
+</script>
+
+</html>
