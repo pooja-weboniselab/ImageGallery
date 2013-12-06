807c401475086068185f48ad6decba5570d7c99d
pooja-weboniselab <pooja.sinha@weboniselab.com>
2013-12-06 12:53:26 +0530

    ajax call to create album

--- newalbum.php
+++ newalbum.php
@@ -5,7 +5,7 @@ $dbHost = "localhost";
 $dbUser = "poojawebonise";
 $dbPass = "weboniselab";
 $dbName = "imagegallery_db";
-echo $album ;
+//echo $album ;
 /* connecting databases */
 $mysql = mysql_connect($dbHost, $dbUser, $dbPass);
 mysql_select_db($dbName);
@@ -13,12 +13,12 @@ $insertAlbum = "insert into albummaster(id,name,status,coverId,created_date,dele
                  values(0,'$album',0,0,'2013-12-5','','')";
 //echo $insertAlbum ;
 if(mysql_query($insertAlbum)){
-    echo "true " ;
-    $lastquery = "select max(id) from albummaster" ;
+    //echo "true " ;
+    $lastquery = "select max(id)  from albummaster" ;
     $result = mysql_query($lastquery);
     $albumvalue=mysql_fetch_array( $result,MYSQL_ASSOC);
-
-    $query = "select * from albummaster where id=$albumvalue" ;
+    $ID = $albumvalue["max(id)"];
+    $query = "select * from albummaster where id=$ID" ;
     $testData=  mysql_query($query);
 
     $data = array();
@@ -29,9 +29,9 @@ if(mysql_query($insertAlbum)){
     }
     $output = '' ;
     foreach($data as $val){
-        echo $val['name'];
-        $output .=  "<li  id='".$val['id']."'><a href='#'><img src='' alt='' class='thumb' />".$val['name']."</a>
-                </li> " ;
+        //echo $val['name'];
+        $output .=  "<li id='".$val['id']."'><a href='#' ><img src='album.jpg' alt='".$val['name']."' class='thumb' /><a href='#'><i class='icon-remove-sign'></i></a></i><input type='checkbox' value=''> </a>".$val['name']."
+       </li> " ;
     }
 
 }
